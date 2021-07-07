0134722e1e2d6857f8afe1787faf8346ec63100c
Leila Dugarcein <dugarcein.leila16@gmail.com>
2021-07-07 09:49:10 +0200

    Canvas problem UI

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -17,14 +17,15 @@ public class UIManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        SceneManager.LoadSceneAsync("MainLevel");
     }
 
     public void Scene1()
     {
-        SceneManager.LoadScene("MainLevel 1");
+        SceneManager.LoadScene("MainLevel");
         Object[0].SetActive(false);
         Object[1].SetActive(false);
+        Object[7].SetActive(true);
 
     }
     public void Exit()
@@ -35,14 +36,22 @@ public class UIManager : MonoBehaviour
     public void PanelRecap(bool PanelTrue)
     {
         if (PanelTrue == true)
+        {
+            Object[7].SetActive(false);
             Object[2].SetActive(true);
 
+        }
+
         else
-        Object[2].SetActive(false);
-        Object[3].SetActive(false);
-        Object[4].SetActive(false);
-        Object[5].SetActive(false);
-        Object[6].SetActive(false);
+        {
+            Object[2].SetActive(false);
+            Object[3].SetActive(false);
+            Object[4].SetActive(false);
+            Object[5].SetActive(false);
+            Object[6].SetActive(false);
+
+            Object[7].SetActive(true);
+        }
 
     }
 
@@ -50,8 +59,9 @@ public class UIManager : MonoBehaviour
     {
         Object[2].SetActive(false);
         Object[6].SetActive(true);
-        Object[3].SetActive(true); 
-         
+        Object[3].SetActive(true);
+       
+
     }
 
     public void Manuel()
